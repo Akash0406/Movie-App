40b7f9b5e108d438bc44055c6dc88b3e80c3a49e
Akash0406 <starx0406@gmail.com>
2022-03-24 16:14:10 +0530

    Update store state

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,43 @@ import Navbar from "./Navbar"
 import { data } from '../data'
 import MovieCard from './MovieCard'
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab"> Movies </div>
-          <div className="tab">Favourites</div>
-        </div>
-        <div className="list">
-          {data.map(movie => (
-            <MovieCard movie={movie} />
-          ))}
+class App extends React.Component {
+
+  componentDidMount() {
+    const { store } = this.props;
+    store.subscribe(() => {
+      console.log('Updated');
+      this.forceUpdate();
+    })
+    store.dispatch({
+      type: 'ADD_MOVIES',
+      movies: data
+    });
+
+    console.log('state', this.props.store.getState());
+
+  }
+
+  render() {
+    const movie = this.props.store.getState();
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab"> Movies </div>
+            <div className="tab">Favourites</div>
+          </div>
+          <div className="list">
+            {movie.map((movie, index) => (
+              <MovieCard movie={movie} key={`movies-${index}`} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css'
 import App from './components/App';
+import movies from './reducers';
+
+
+const store = createStore(movies);
+
+// console.log('Before Store', store.getState());
+
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies: [{ name: 'superman' }]
+// })
+
+// console.log('After Store', store.getState());
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
